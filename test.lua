0334d0a699482dd2c2bce7f7a881f9662d898f63
Christopher Moore <christopher.e.moore@gmail.com>
2016-11-13 20:29:39 +0200

    verified with divergence-free fields

--- test.lua
+++ test.lua
@@ -14,11 +14,22 @@ a_ij = 1/h^2 -4/h^2 1/h^2
 
 local matrix = require 'matrix'
 
+local function time(f, ...)
+	local t1 = os.clock()
+	f(...)
+	local t2 = os.clock()
+	return t2 - t1
+end
+
 local function GaussSeidel(h, u, f)
-	local L = #u - 2
-	for i=2,L+1 do
-		for j=2,L+1 do
-			local askew_u = (u[i+1][j] + u[i-1][j] + u[i][j+1] + u[i][j-1]) / h^2
+	local L = #u
+	for i=1,L do
+		for j=1,L do
+			local u_xl = i > 1 and u[i-1][j] or 0--u[i][j]
+			local u_xr = i < L and u[i+1][j] or 0--u[i][j]
+			local u_yl = j > 1 and u[i][j-1] or 0--u[i][j]
+			local u_yr = j < L and u[i][j+1] or 0--u[i][j]
+			local askew_u = (u_xl + u_xr + u_yl + u_yr) / h^2
 			local adiag = -4 / h^2
 			u[i][j] = (f[i][j] - askew_u) / adiag
 		end
@@ -26,66 +37,78 @@ local function GaussSeidel(h, u, f)
 end
 
 local accuracy = 1e-10
-local nSmooth = 3
-local function twoGrid(h, u, f)
-	local L = #u - 2
+local function twoGrid(h, u, f, smooth)
+	local L = #u
 	
 	if L == 1 then
 		-- Gauss-Seidel single-cell update 
-		local askew_u = (u[1][2] + u[3][2] + u[2][1] + u[2][3]) / h^2
+		local u_xl, u_xr, u_yl, u_yr
+		if border == 'dirichlet' then	-- constant-valued
+			u_xl = border_u_xl
+			u_xr = border_u_xr
+			u_yl = border_u_yl
+			u_yr = border_u_yr
+		else	-- periodic? neumann (constant-derivative) with deriv=0?
+			local tmp = 0--u[1][1]
+			u_xl, u_xr, u_yl, u_yr = tmp, tmp, tmp, tmp
+		end
+		local askew_u = (u_xl + u_xr + u_yl + u_yr) / h^2
 		local adiag = -4 / h^2
-		u[2][2] = (f[2][2] - askew_u) / adiag 
+		u[1][1] = (f[1][1] - askew_u) / adiag 
 		return
 	end
 
-	local nPreSmooth = nSmooth
-	for i=1,nPreSmooth do
+	for i=1,smooth do
 		GaussSeidel(h, u, f)
 	end
 
-	-- r = del^2 u + f
-	local r = matrix.zeros(L+2,L+2)
-	for i=2,L+1 do
-		for j=2,L+1 do
-			local askew_u = (u[i+1][j] + u[i-1][j] + u[i][j+1] + u[i][j-1]) / h^2
+	-- r = f - del^2 u
+	local r = matrix.zeros(L,L)
+	for i=1,L do
+		for j=1,L do
+			local u_xl = i > 1 and u[i-1][j] or 0--u[i][j]
+			local u_xr = i < L and u[i+1][j] or 0--u[i][j]
+			local u_yl = j > 1 and u[i][j-1] or 0--u[i][j]
+			local u_yr = j < L and u[i][j+1] or 0--u[i][j]
+			local askew_u = (u_xl + u_xr + u_yl + u_yr) / h^2
 			local adiag = -4 / h^2
-			r[i][j] = f[i][j] - (askew_u + adiag * u[i][j])
+			local a_u = askew_u + adiag * u[i][j]
+			r[i][j] = f[i][j] - a_u
 		end
 	end
 	
 	-- del^2 V = R
 	local L2 = L/2
-	local R = matrix.zeros(L2+2, L2+2)
-	for I=2,L2+1 do
-		local i = 2*I-2
-		for J=2,L2+1 do
-			local j=2*J-2
+	local R = matrix.zeros(L2, L2)
+	for I=1,L2 do
+		local i = 2*I-1
+		for J=1,L2 do
+			local j=2*J-1
 			R[I][J] = (r[i][j] + r[i+1][j] + r[i][j+1] + r[i+1][j+1]) / 4
 		end
 	end
 
-	local V = matrix.zeros(L2+2, L2+2)
-	twoGrid(2*h, V, R)
+	local V = matrix.zeros(L2, L2)
+	twoGrid(2*h, V, R, smooth)
 
-	local v = matrix.zeros(L+2, L+2)
-	for I=2,L2+1 do
-		local i=2*I-2
-		for J=2,L2+1 do
-			local j=2*J-2
+	local v = matrix.zeros(L, L)
+	for I=1,L2 do
+		local i=2*I-1
+		for J=1,L2 do
+			local j=2*J-1
 			local value = V[I][J]
 			v[i][j], v[i+1][j], v[i][j+1], v[i+1][j+1] = value, value, value, value
 		end
 	end
 
 	-- correct u
-	for i=2,L+1 do
-		for j=2,L+1 do
+	for i=1,L do
+		for j=1,L do
 			u[i][j] = u[i][j] + v[i][j]
 		end
 	end
 
-	local postSmooth = nSmooth
-	for i=1,postSmooth do
+	for i=1,smooth do
 		GaussSeidel(h, u, f)
 	end
 end
@@ -108,18 +131,77 @@ local function relativeError(psi, psiNew)
 	return err
 end
 
+local function amrsolve(f, h)
+	f = matrix(f)
+	local smooth = 7	-- 7 is optimal time for me
+	local psi = -f
+	for iter=1,math.huge do
+		local psiNew = matrix(psi)
+		twoGrid(h, psi, f, smooth)
+		local frobErr = (psi - psiNew):norm()
+		local relErr = relativeError(psi, psiNew)
+		print(iter,'relErr', relErr, 'frob', frobErr)
+		if relErr < accuracy or not math.isfinite(relErr) then break end
+	end
+	return psi
+end
 
+-- m is a matrix of [M][N][2]
+-- returns a matrix of [M][N]
+-- boundary condition is constant
+local function div(m)
+	return matrix.lambda({#m, #m[1]}, function(...)
+		local ix, iy = ...
+		local ixp = math.min(ix+1, #m)
+		local ixn = math.max(ix-1, 1)
+		local iyp = math.min(iy+1, #m[1])
+		local iyn = math.max(iy-1, 1)
+		return (m[ixp][iy][1] - m[ixn][iy][1]) / (ixp - ixn) 
+			+ (m[ix][iyp][2] - m[ix][iyn][2]) / (iyp - iyn)
+	end)
+end
+
+-- psi is a matrix of [M][N]
+-- return a matrix of [M][N][2]
+-- boundary condition is constant
+local function del(psi)
+	return matrix.lambda({#psi, #psi[1]}, function(...)
+		local ix, iy, z = ...
+		local ixp = math.min(ix+1, #psi)
+		local ixn = math.max(ix-1, 1)
+		local iyp = math.min(iy+1, #psi[1])
+		local iyn = math.max(iy-1, 1)
+		return matrix{
+			(psi[ixp][iy] - psi[ixn][iy]) / (ixp - ixn),
+			(psi[ix][iyp] - psi[ix][iyn]) / (iyp - iyn),
+		}
+	end)
+end
 
 local log2L = ... and tonumber(...) or 6
 local L = bit.lshift(1,log2L)
 local h = 1 / (L + 1)
-local size = matrix{L+2,L+2}
-local center = (size+1)/2
+local size = matrix{L,L}
+local center = size/2
 
+-- [[ using a real vector field
+-- E = div psi + div x B
+local E = matrix.zeros(L,L,2)
+for i=1,L do
+	for j=1,L do
+		local x = (matrix{i+.5,j+.5} - center) * h
+		E[i][j] = -x / x:norm()
+	end
+end
+-- f = div.E = div^2 psi
+local f = div(E) / h
+--]]
+
+--[=[ manual specify f
 local f = matrix.lambda(size, function(...)
 	local i = matrix{...}
 	
-	-- [[ electricity: del^2 V = -rho / epsilon_0
+	--[[ electricity: del^2 V = -rho / epsilon_0
 	local charge = 1e+6
 	local epsilon0 = 1
 	local Q = -charge / epsilon0
@@ -130,19 +212,32 @@ local f = matrix.lambda(size, function(...)
 	local G = 1
 	local Q = 4 * math.pi * G * mass 
 	--]]
-	
+
 	return (i - center):normLInf() < 1 and Q or 0
 end)
-local psi = -f
+--]=]
 
-for iter=1,math.huge do
-	local psiNew = matrix(psi)
-	twoGrid(h, psi, f)
-	local relErr = relativeError(psi, psiNew)
-	print(iter,'rel err',relErr)
-	if relErr < accuracy then break end
-end
+-- psi = div^-2 f
+local psi = amrsolve(f, h)
 
+print('div E frob norm',(div(E)/h):norm())
+
+-- div-free E = del psi
+local divfree_E = del(psi) / h
+
+print('div vs divfree', (E - divfree_E):norm())
+
+local divfree_f = div(divfree_E) / h
+
+print('divfree E frob norm', divfree_f:norm())
+
+local divfree_psi = amrsolve(divfree_f, h)
+
+local divfree2_E = del(divfree_psi) / h
+
+print('divfree vs divfree2', (divfree_E - divfree2_E):norm())
+
+local _ = require 'matrix.index'
 require 'gnuplot'{
 	persist = true,
 	style = 'data lines',
@@ -152,7 +247,21 @@ require 'gnuplot'{
 		y=range(size[2]),
 		f,
 		psi,
+		E(_,_,1),
+		E(_,_,2),
+		divfree_E(_,_,1),
+		divfree_E(_,_,2),
 	},
 	--{splot=true, using='1:2:3', title='f'},
-	{splot=true, using='1:2:4', title='psi'},
+	--{splot=true, using='1:2:4', title='psi'},
+	
+	-- maximum magnitude is 1
+	--{splot=true, using='1:2:5', title='Ex'},
+	{splot=true, using='1:2:6', title='Ey'},
+	--{splot=true, using='1:2:5:6', 'with vectors', title='E'},
+	
+	-- this should be on the order of E as well, but instead it's 1e-7
+	--{splot=true, using='1:2:7', title='divfree-Ex'},
+	{splot=true, using='1:2:8', title='divfree-Ey'},
+	--{splot=true, using='1:2:7:8', 'with vectors', title='divfree-E'},
 }
