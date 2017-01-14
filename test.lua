7c53bdcaf257dcaf56d6721fe7de332b2bc4d5da
Christopher Moore <christopher.e.moore@gmail.com>
2017-01-14 16:41:54 +0530

    Forgot to add

---
+++ test.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env lua
+require 'ext'
+-- only works up to (2^6)^2 before it gets too slow
+-- (2^6)^2 takes about 5 seconds to run, so don't do too many samples
+for _,test in ipairs{
+	'cpu.lua', 
+	'gpu-obj.lua',
+} do
+	print('testing '..test..'...')
+	for i=0,5 do
+		local bestTime = math.huge
+		for try=0,10 do
+			local time = assert(tonumber(io.readproc('./'..test..' '..i):match'time taken: (.*)'))
+			bestTime = math.min(bestTime, time)
+		end
+		print('(2^'..i..')^2 best time '..bestTime)
+	end
+end
