5ed9aa4a1199dcc8536f938d0cb8d7dc5ed114db
Christopher Moore <christopher.e.moore@gmail.com>
2018-03-27 08:29:21 +0200

    more

--- test.lua
+++ test.lua
@@ -2,20 +2,42 @@
 require 'ext'
 -- only works up to (2^6)^2 before it gets too slow
 -- (2^6)^2 takes about 5 seconds to run, so don't do too many samples
-for _,test in ipairs{
-	'test-cpu.lua',
-	'test-cpu-raw.lua', 
-	'test-gpu.lua',
-	--'test-gpu-obj.lua',
-	'test-cpu-gpu.lua',
-} do
-	print('testing '..test..'...')
-	for i=0,5 do
+local cols = {
+	'cpu',
+	'cpu-raw',
+	'gpu',
+	--'gpu-obj',
+	'cpu-gpu',
+}
+
+local f = io.open('out.txt', 'w')
+
+f:write'#size'
+for _,col in ipairs(cols) do
+	f:write('\t', col)
+end
+f:write'\n'
+
+local tries = 10
+
+for size=0,10 do
+	f:write(2^size)
+	for _,col in ipairs(cols) do
+		local test = 'test-'..col..'.lua'
 		local bestTime = math.huge
-		for try=0,10 do
-			local time = assert(tonumber(io.readproc('./'..test..' '..i):match'time taken: (.*)'))
+		for try=1,tries do
+			assert(os.execute('./'..test..' '..size..' 2> tmp > /dev/null'))
+			local results = file.tmp
+			file.tmp = nil
+			local time = assert(tonumber(results:match'time taken: (.*)'), 
+				"failed to find time taken in output:\n"
+				..('='):rep(20)..'\n'
+				..results)
 			bestTime = math.min(bestTime, time)
 		end
-		print('(2^'..i..')^2 best time '..bestTime)
+		f:write('\t', bestTime)
 	end
+	f:write'\n'
 end
+
+f:close()
